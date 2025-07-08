ed0b494825fd0fa355d4141f89dd273a02f06906
joaquincasarino85 <joaquincasarino85@gmail.com>
2025-07-08 17:24:23 -0300

    configurations

--- react-frontend/start-entry.sh
+++ react-frontend/start-entry.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$FRONTEND_ENV" = "prod" ]; then
   echo "Iniciando frontend en modo PRODUCCIÓN"
   npm run build
-  serve -s dist -l 0.0.0.0:${PORT:-8080}
+  serve -s dist -l ${PORT:-8080}
 else
   echo "Iniciando frontend en modo DESARROLLO"
   npm run dev -- --host 0.0.0.0 --port 5173
